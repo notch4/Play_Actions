28700df7ec00901d0b9d0ea44c0e1e78a1345410
yubraj pokharel <yubraj.pokharel555@gmail.com>
2016-03-23 02:13:14 -0500

    addding models

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,44 @@
 package controllers
 
+import models.Artist
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.main())
+    Redirect(routes.Application.listArtist)
   }
+
+  def listArtist = Action {
+    Ok(views.html.home(Artist.fetch))
+  }
+
+  def fetchArtistByName(name: String) = Action{
+    Ok(views.html.home(Artist.fetchByName(name)))
+  }
+
+  def search(name: String, country: String) = Action {
+    val artists = Artist.fetchByNameOrCountry(name, country)
+    if(artists.isEmpty){
+      NoContent
+    }
+    else{
+      Ok(views.html.home(artists))
+    }
+  }
+
+  def search2(name: Option[String], country: String) = Action {
+    val result = name match{
+      case Some(n) => Artist.fetchByNameOrCountry(n, country)
+      case None => Artist.fetchByCountry(country)
+    }
+    if(result.isEmpty){
+      NoContent
+    }
+    else {
+      Ok(views.html.home(result))
+    }
+  }
+
 
 }
